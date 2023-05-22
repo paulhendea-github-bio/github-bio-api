d0eb134089d8e8464c91934334c7f0342ba2a265
Paul Hendea <paulhendea@outlook.com>
2023-05-22 22:07:11 +0200

    working on ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { OpenAiModule } from './openai/openai.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EmojiModule } from './emoji/emoji.module';
 
 @Module({
-  imports: [],
+  imports: [
+    OpenAiModule,
+    EmojiModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
 })
 export class AppModule {}
